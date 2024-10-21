964bfc08c95fe1a66d11c3ce60c43d73d5749f38
esteban gamboa <estebitangb12@gmail.com>
2024-10-21 11:35:50 +0200

    ventana modal de iniciar sesion

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
 window.onload=function(){
     window.addEventListener("scroll",moverCaja);
+    document.querySelector(".iniciarSesion").addEventListener("click",abrirVentanaSesion);
+    document.querySelector(".cerrar").addEventListener("click",cerrarVentanaSesion);
 }
 function moverCaja(){
     let caja=document.querySelector(".aside2")
     let scrollTop=window.scrollY;//posicion scroll
     console.log(scrollTop);
     caja.style.top = `(${200+scrollTop}px`; // Mueve la caja con el scroll
+}
+function abrirVentanaSesion(){
+
+    let modal=document.querySelector(".modalIniciarSesion");
+    modal.style.display="flex";
+}
+function cerrarVentanaSesion(){
+    let modal=document.querySelector(".modalIniciarSesion");
+    modal.style.display="none";
 }
