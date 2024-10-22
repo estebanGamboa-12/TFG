0558886c92e711fff8b836611f3679955c85c744
esteban gamboa <estebitangb12@gmail.com>
2024-10-22 12:18:59 +0200

    vistas responsive, inicio, logeado

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 window.onload=function(){
     document.querySelector(".iniciarSesion").addEventListener("click",abrirVentanaSesion);
     document.querySelector(".cerrar").addEventListener("click",cerrarVentanaSesion);
+    document.querySelector(".bars").addEventListener("click",mostrarAside);
+    document.addEventListener("click",cerrarAsideFuera);
 }
  
 function abrirVentanaSesion(){
@@ -11,5 +13,21 @@ function abrirVentanaSesion(){
 function cerrarVentanaSesion(){
     let modal=document.querySelector(".modalIniciarSesion");
     modal.style.display="none";
+}
+function mostrarAside(){
+    let aside=document.querySelector('.contenido-aside');
+    let section=document.querySelector("section");
+    section.classList.add("fondo");
+    aside.style.display="block";
+}
+function cerrarAsideFuera(event){
+    let aside=document.querySelector('.contenido-aside');
+    let bars=document.querySelector(".bars");
+    let section=document.querySelector("section");
+
+    if(!aside.contains(event.target) && !bars.contains(event.target)){
+        aside.style.display="none";
+        section.classList.remove("fondo");
+    }
 }
 
